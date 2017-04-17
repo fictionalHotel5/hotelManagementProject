e75944dbe9dd0b25d7a3281407ee151e02985d7d
fictionalHotel5 <fictionalhotelteam@hotmail.com>
2017-04-17 19:48:53 +0200

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,44 @@
+package hotelMenagment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+/**
+ * 
+ * @author Sipo
+ *
+ */
+public final class Admin {
+	//pojednostavljena klasa radi lakseg citanja koda
+	private String username;
+	private String password;
+
+	// constructor with data fields
+	/**
+	 * @param username
+	 * @param password
+	 */
+	
+	public Admin(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public Admin() {
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+}
