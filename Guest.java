f9c0f371611bffa31178feed8d2c49d936458376
MAXDATA <MAXDATA@MAXDATA-PC>
2017-04-22 18:58:42 +0200

    Builder :(

--- Guest.java
+++ Guest.java
@@ -1,11 +1,6 @@
 
-
 import java.sql.Date;
-import java.sql.SQLException;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+
 
 public class Guest {
 
@@ -21,9 +16,18 @@ public class Guest {
 	private String sex;
 	private int age;
 	private int roomNumber;
-	 private String roomType;
+	private String roomType;
 	private Date timeCheckedIn;
+	private double balance;
+	private int numOfDays;
+	private int timesGymUsed;
+	private int timesPoolUsed;
+	private int timesRestaurantUsed;
+	private int timesSaunaUsed;
+	private int timesCinemaUsed;
+	private boolean isCheckedIn;
 
+	
 	public Guest(Builder builder) {
 		this.firstName = builder.firstName;
 		this.lastName = builder.lastName;
@@ -34,8 +38,73 @@ public class Guest {
 		this.age = builder.age;
 		this.roomNumber = builder.roomNumber;
 		this.roomType = builder.roomType;
+		this.balance = builder.balance;
+		this.numOfDays = builder.numOfDays;
+		this.timesGymUsed = builder.timesGymUsed;
+		this.timesPoolUsed = builder.timesPoolUsed;
+		this.timesRestaurantUsed = builder.timesRestaurantUsed;
+		this.timesSaunaUsed = builder.timesSaunaUsed;
+		this.timesCinemaUsed = builder.timesCinemaUsed;
 		this.timeCheckedIn = builder.timeCheckedIn;
+		this.isCheckedIn=builder.isCheckedIn;
 	}
+
+	/**getters */
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getIdNumber() {
+		return idNumber;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getRoomType() {
+		return roomType;
+	}
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	public Date getTimeCheckedIn() {
+		return timeCheckedIn;
+	}	
+	public double getBalance() {
+		return balance;
+	}
+	public int getNumOfDays() {
+		return numOfDays;
+	}
+	public int getTimesGymUsed() {
+		return timesGymUsed;
+	}
+	public int getTimesPoolUsed() {
+		return timesPoolUsed;
+	}
+	public int getTimesRestaurantUsed() {
+		return timesRestaurantUsed;
+	}
+	public int getTimesSaunaUsed() {
+		return timesSaunaUsed;
+	}
+	public int getTimesCinemaUsed() {
+		return timesCinemaUsed;
+	}
+	public boolean isCheckedIn() {
+		return isCheckedIn;
+	}
+
 
 	public static class Builder {
 
@@ -48,96 +117,97 @@ public class Guest {
 		private String roomType;
 		private int age;
 		private int roomNumber;
+		private double balance;
+		private int numOfDays;
+		private int timesGymUsed;
+		private int timesPoolUsed;
+		private int timesRestaurantUsed;
+		private int timesSaunaUsed;
+		private int timesCinemaUsed;
+		private boolean isCheckedIn;	
 		private Date timeCheckedIn;
 
 		public Builder() {
 
 		}
-
-		private String getFirstName() {
-			return firstName;
-		}
-
-		private String getLastName() {
-			return lastName;
-		}
-
-		private String getUserName() {
-			return userName;
-		}
-
-		private String getPassword() {
-			return password;
-		}
-
-		private String getIdNumber() {
-			return idNumber;
-		}
-
-		private String getSex() {
-			return sex;
-		}
-
-		private String getRoomType() {
-			return roomType;
-		}
-
-		private int getAge() {
-			return age;
-		}
-
-		private int getRoomNumber() {
-			return roomNumber;
-		}
-
-		private Date getTimeCheckedIn() {
-			return timeCheckedIn;
-		}
-
+	
+		/**setters*/
 		public Builder firstName(String firstName) {
 			this.firstName = firstName;
 			return this;
-		}
-
+		}		
 		public Builder lastName(String lastName) {
 			this.lastName = lastName;
 			return this;
-		}
-
+		}		
 		public Builder userName(String userName) {
 			this.userName = userName;
 			return this;
-		}
-
+		}	
 		public Builder password(String password) {
 			this.password = password;
 			return this;
-		}
-
+		}	
 		public Builder idNumber(String idNumber) {
 			this.idNumber = idNumber;
 			return this;
-		}
-
+		}	
 		public Builder sex(String sex) {
 			this.sex = sex;
 			return this;
 		}
-
-		public Builder roomNumber(int room) {
-			this.roomNumber = room;
+		public Builder age(int age){
+			this.age = age;
 			return this;
 		}
 		public Builder roomType(String type) {
 			this.roomType = type;
 			return this;
+		}	
+		public Builder roomNumber(int room) {
+			this.roomNumber = room;
+			return this;
 		}
 
-		public Builder timeCheckedIn(Date checkedIn) {
+		public Builder timeCheckedIn(Date checkedIn){ 
 			this.timeCheckedIn = checkedIn;
 			return this;
-
 		}
+		public Builder isCheckedIn(boolean isChecked){	
+		    this.isCheckedIn = isChecked;
+			return this;
+		}
+		public Builder balance(double balance){
+			this.balance = balance;
+			return this;
+		}
+		public Builder numOfDays(int numOfDays){
+			this.numOfDays = numOfDays;
+			return this;
+		}
+		public Builder timesGymUsed(int timesGymUsed){
+			this.timesGymUsed= timesGymUsed;
+			return this;
+		}
+		public Builder timesPoolUsed(int timesPoolUsed){
+			this.timesPoolUsed= timesPoolUsed;
+			return this;
+		}
+		public Builder timesSaunaUsed(int timesSaunaUsed){
+			this.timesSaunaUsed= timesSaunaUsed;
+			return this;
+		}
+		public Builder timesRestaurantUsed(int timesRestaurantUsed){
+			this.timesRestaurantUsed= timesRestaurantUsed;
+			return this;
+		}
+		public Builder timesCinemaUsed(int timesCinemaUsed){
+			this.timesCinemaUsed= timesCinemaUsed;
+			return this;
+		}
+		
+
+		/** @ Jasmina */
 
 		public Guest build() {
 			if(firstName==null){
@@ -158,13 +228,16 @@ public class Guest {
 			if(sex==null){
 				throw new IllegalStateException( " No sex!");
 			}
-			
-			/**missing age and roomNumber*/
-			
+			if (age <18 || age >120 ){
+				throw new IllegalStateException( " Age out of range!");
+			}
+			if (roomNumber < 1 ){
+				throw new IllegalStateException( " Room number does not exists!");
+			}
 			if(roomType==null){
 				throw new IllegalStateException( " No room type!");
 			}
-			
+		
 			if(timeCheckedIn==null){
 				throw new IllegalStateException( " No time checked in!");
 			}
